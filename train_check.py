9cc18d9a92c48b36b0cf97ffffec020d3d9542a8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-04 22:06:48 +0900

    Update train_check.py

--- train_check.py
+++ train_check.py
@@ -5,11 +5,18 @@ from model.tokenizer import load_tokenizer
 from model.lora import LoRANetwork
 from data.mvtec import MVTecDRAEMTrainDataset
 from diffusers.optimization import SchedulerType, TYPE_TO_SCHEDULER_FUNCTION
+from diffusers import DDPMScheduler
 from accelerate import Accelerator
 from utils import prepare_dtype
+from utils.model_utils import get_noise_noisy_latents_and_timesteps
+from attention_store import AttentionStore
 from utils.pipeline import AnomalyDetectionStableDiffusionPipeline
 from utils.scheduling_utils import get_scheduler
-
+from tqdm import tqdm
+from utils.attention_control import register_attention_control
+from utils import get_epoch_ckpt_name, save_model
+import time
+import json
 
 def main(args) :
 
@@ -18,11 +25,21 @@ def main(args) :
     args.logging_dir = os.path.join(output_dir, 'log')
     os.makedirs(output_dir, exist_ok=True)
     os.makedirs(args.logging_dir, exist_ok=True)
+    record_save_dir = os.path.join(output_dir, 'record')
+    os.makedirs(record_save_dir, exist_ok=True)
+    with open(os.path.join(record_save_dir, 'config.json'), 'w') as f:
+        json.dump(vars(args), f, indent=4)
 
     print(f'\n step 2. model')
     tokenizer = load_tokenizer(args)
     text_encoder, vae, unet = load_SD_model(args)
+    vae_scale_factor = 0.18215
     network = LoRANetwork(text_encoder=text_encoder, unet=unet, lora_dim = args.network_dim, alpha = args.network_alpha)
+    noise_scheduler = DDPMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear",
+                                                                            num_train_timesteps=1000, clip_sample=False)
+    print(f' (2.2) attn controller')
+    controller = AttentionStore()
+    register_attention_control(unet, controller)
 
     print(f'\n step 3. optimizer')
     trainable_params = network.prepare_optimizer_params(args.text_encoder_lr, args.unet_lr, args.learning_rate)
@@ -32,11 +49,17 @@ def main(args) :
     obj_dir = os.path.join(args.data_path, args.obj_name)
     train_dir = os.path.join(obj_dir, "train")
     root_dir = os.path.join(train_dir, "good/rgb")
-    dataset = MVTecDRAEMTrainDataset(root_dir= root_dir,
-                                     #anomaly_source_path=args.anomaly_source_path,
-                                     anomaly_source_path=args.data_path,
-                                     resize_shape=[512,512],
-                                     tokenizer=tokenizer,)
+    if args.general_training :
+        caption = 'good'
+        args.anomaly_source_path = os.path.join(train_dir, "anomal_general")
+    else :
+        caption = args.obj_name
+        args.anomaly_source_path = os.path.join(train_dir, "anomal")
+    dataset = MVTecDRAEMTrainDataset(root_dir=root_dir,
+                                     anomaly_source_path=args.anomaly_source_path,
+                                     resize_shape=[512, 512],
+                                     tokenizer=tokenizer,
+                                     caption = caption,)
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=16)
 
     print(f'\n step 5. lr')
@@ -48,8 +71,19 @@ def main(args) :
 
     print(f'\n step 6. accelerator and device')
     weight_dtype, save_dtype = prepare_dtype(args)
+
+    if args.log_with in ["wandb", "all"]:
+        try:
+            import wandb
+        except ImportError:
+            raise ImportError("No wandb / wandb がインストールされていないようです")
+        os.environ["WANDB_DIR"] = args.logging_dir
+        if args.wandb_api_key is not None:
+            wandb.login(key=args.wandb_api_key)
     accelerator = Accelerator(gradient_accumulation_steps=args.gradient_accumulation_steps,
-                            mixed_precision=args.mixed_precision, log_with=args.log_with,project_dir=args.logging_dir,)
+                              mixed_precision=args.mixed_precision,
+                              log_with=args.log_with,
+                              project_dir=args.logging_dir,)
     is_main_process = accelerator.is_main_process
     vae.requires_grad_(False)
     vae.to(dtype=weight_dtype)
@@ -61,65 +95,60 @@ def main(args) :
     network.apply_to(text_encoder, unet, True, True)
     if args.network_weights is not None:
         network.load_weights(args.network_weights)
-
     if args.train_unet and args.train_text_encoder:
         unet, text_encoder, network, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(
-                                                    unet, text_encoder, network, optimizer, dataloader, lr_scheduler)
+                                                 unet, text_encoder, network, optimizer, dataloader, lr_scheduler)
     elif args.train_unet:
         unet, network, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(unet, network, optimizer,
                                                                                        dataloader, lr_scheduler)
         text_encoder.to(accelerator.device,dtype=weight_dtype)
     elif args.train_text_encoder:
         text_encoder, network, optimizer, train_dataloader, lr_scheduler = accelerator.prepare(text_encoder, network,
-                                                                                    optimizer, dataloader, lr_scheduler)
+                                                                                 optimizer, dataloader, lr_scheduler)
         unet.to(accelerator.device,dtype=weight_dtype)
 
-    print(f'text encode deivce : {text_encoder.device} , unet device : {unet.device} network device : {network.device}')
+    print(f'\n step 7. Train!')
+    progress_bar = tqdm(range(args.max_train_steps), smoothing=0,
+                        disable=not accelerator.is_local_main_process, desc="steps")
+    global_step = 0
+    loss_list = []
+    for epoch in range(0, args.num_epochs):
+        epoch_loss_total = 0
+        accelerator.print(f"\nepoch {epoch + 1}/{args.num_epochs}")
 
-    print(f'\n step 7. inference check')
-    scheduler_cls = get_scheduler(args.sample_sampler, False)[0]
-    scheduler = scheduler_cls(num_train_timesteps=args.scheduler_timesteps,
-                              beta_start=args.scheduler_linear_start,
-                              beta_end=args.scheduler_linear_end,
-                              beta_schedule=args.scheduler_schedule)
-    pipeline = AnomalyDetectionStableDiffusionPipeline(vae=vae,
-                                                       text_encoder=text_encoder,
-                                                       tokenizer=tokenizer,
-                                                       unet=unet,
-                                                       scheduler=scheduler,
-                                                       safety_checker=None,
-                                                       feature_extractor=None,
-                                                       requires_safety_checker=False,
-                                                       random_vector_generator=None,
-                                                       trg_layer_list=None)
-
-    anomal_concepts = ['crack', 'cut', 'hole', 'contaminate', 'crash', 'dirty', 'bad', 'torn', 'crumpled',
-                       'deformed']
-    args.anomal_base_dir = os.path.join(train_dir, 'anomal')
-    os.makedirs(args.anomal_base_dir, exist_ok=True)
-    for adjective in anomal_concepts:
-        adjective_base_folder = os.path.join(args.anomal_base_dir, adjective)
-        os.makedirs(adjective_base_folder, exist_ok=True)
-        gen_caption = f'the concept of {adjective}'
-        for i in range(100) :
-            latents = pipeline(prompt=gen_caption,
-                               height=512, width=512,
-                               num_inference_steps=args.num_ddim_steps,
-                               guidance_scale=args.guidance_scale,
-                               negative_prompt=args.negative_prompt,)
-            recon_image = pipeline.latents_to_image(latents[-1])[0].resize((512,512))
+        accelerator.wait_for_everyone()
+        ### 4.2 sampling
+        if is_main_process :
+            ckpt_name = get_epoch_ckpt_name(args, "." + args.save_model_as, epoch + 1)
+            unwrapped_nw = accelerator.unwrap_model(network)
+            save_model(args, ckpt_name, unwrapped_nw, save_dtype)
+            scheduler_cls = get_scheduler(args.sample_sampler, False)[0]
+            scheduler = scheduler_cls(num_train_timesteps=args.scheduler_timesteps,
+                                      beta_start=args.scheduler_linear_start, beta_end=args.scheduler_linear_end,
+                                      beta_schedule=args.scheduler_schedule)
+            pipeline = AnomalyDetectionStableDiffusionPipeline(vae=vae, text_encoder=text_encoder, tokenizer=tokenizer,
+                                        unet=unet, scheduler=scheduler,safety_checker=None, feature_extractor=None,
+                                        requires_safety_checker=False, random_vector_generator=None, trg_layer_list=None)
+            latents = pipeline(prompt='bagel', #batch['caption'],
+                               height=512, width=512,  num_inference_steps=args.num_ddim_steps,
+                               guidance_scale=args.guidance_scale, negative_prompt=args.negative_prompt, )
+            gen_img = pipeline.latents_to_image(latents[-1])[0].resize((512, 512))
             img_save_base_dir = args.output_dir + "/sample"
             os.makedirs(img_save_base_dir, exist_ok=True)
             ts_str = time.strftime("%Y%m%d%H%M%S", time.localtime())
             num_suffix = f"e{epoch:06d}"
             img_filename = (f"{ts_str}_{num_suffix}_seed_{args.seed}.png")
-            gen_img.save(os.path.join(img_save_base_dir, img_filename))
+            gen_img.save(os.path.join(img_save_base_dir,img_filename))
             controller.reset()
+        accelerator.end_training()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Anomal Lora')
+    parser.add_argument('--seed', type=int, default=42)
     parser.add_argument('--output_dir', type=str, default='output')
+    parser.add_argument('--wandb_api_key', type=str,
+                        default='output')
     parser.add_argument('--pretrained_model_name_or_path', type=str,
                         default='facebook/diffusion-dalle')
     parser.add_argument('--network_dim', type=int,default=64)
@@ -132,7 +161,7 @@ if __name__ == '__main__':
     # step 4. dataset and dataloader
     parser.add_argument('--data_path', type=str,
                         default=r'../../../MyData/anomaly_detection/MVTec3D-AD')
-    parser.add_argument('--obj_name', type=str, default='bagel')
+    parser.add_argument('--obj_name', type=str, default='bottle')
     parser.add_argument('--anomaly_source_path', type=str)
     parser.add_argument('--batch_size', type=int, default=1)
     # step 5. lr
@@ -146,7 +175,9 @@ if __name__ == '__main__':
     parser.add_argument("--save_precision",type=str,default=None,choices=[None, "float", "fp16", "bf16"],)
     parser.add_argument("--gradient_accumulation_steps",type=int,default=1,)
     parser.add_argument("--log_with",type=str,default=None,choices=["tensorboard", "wandb", "all"],)
+
     # step 7. inference check
+    parser.add_argument("--max_train_steps", type=int, default=10000)
     parser.add_argument("--sample_sampler",type=str,default="ddim",
                         choices=["ddim","pndm","lms","euler","euler_a","heun","dpm_2","dpm_2_a","dpmsolver",
                                  "dpmsolver++","dpmsingle","k_lms","k_euler","k_euler_a","k_dpm_2","k_dpm_2_a",],)
@@ -160,5 +191,26 @@ if __name__ == '__main__':
     parser.add_argument("--guidance_scale", type=float, default=8.5)
     parser.add_argument("--negative_prompt", type=str,
                         default="low quality, worst quality, bad anatomy, bad composition, poor, low effort")
+    import ast
+    def arg_as_list(arg):
+        v = ast.literal_eval(arg)
+        if type(v) is not list:
+            raise argparse.ArgumentTypeError("Argument \"%s\" is not a list" % (arg))
+        return v
+    parser.add_argument("--do_task_loss", action='store_true')
+    parser.add_argument("--task_loss_weight", type=float, default=0.5)
+    parser.add_argument("--do_dist_loss", action='store_true')
+    parser.add_argument("--dist_loss_weight", type=float, default = 1)
+    parser.add_argument("--do_attn_loss", action='store_true')
+    parser.add_argument("--attn_loss_weight", type=float, default=1.0)
+    parser.add_argument("--do_cls_train", action='store_true')
+    parser.add_argument('--normal_weight', type=float, default=1.0)
+    parser.add_argument('--anormal_weight', type=float, default=1.0)
+    parser.add_argument("--trg_layer_list", type=arg_as_list, )
+    parser.add_argument("--save_model_as",type=str,default="safetensors",
+                        choices=[None, "ckpt", "safetensors", "diffusers", "diffusers_safetensors"],)
+    parser.add_argument("--output_name", type=str, default=None,
+                        help="base name of trained model file / 学習後のモデルの拡張子を除くファイル名")
+    parser.add_argument("--general_training", action='store_true')
     args = parser.parse_args()
     main(args)
