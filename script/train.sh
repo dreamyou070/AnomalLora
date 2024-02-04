8b8ec66b3b62f0db819d70e797043a32f69c74f8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-04 12:30:43 +0900

    a

--- script/train.sh
+++ script/train.sh
@@ -2,4 +2,5 @@
 
 
 python ../train.py \
+ --output_dir ../../result/20240204 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors
--- train.py
+++ train.py
@@ -5,24 +5,27 @@ from model.lora import LoRANetwork
 def main(args) :
 
     print(f' step 1. setting')
-    if not os.path.exists(args.log_path):
-        os.makedirs(args.log_path)
+    output_dir = args.output_dir
+    log_dir = os.path.join(output_dir, 'log')
+    os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(log_dir, exist_ok=True)
+
 
     print(f' step 2. model')
     text_encoder, vae, unet, _ = load_SD_model(args)
-    network = LoRANetwork(text_encoder=text_encoder,
-                          unet=unet,
-                          lora_dim = args.network_dim,
-                          alpha = args.network_alpha,)
+    network = LoRANetwork(text_encoder=text_encoder, unet=unet, lora_dim = args.network_dim, alpha = args.network_alpha,)
 
     print(f' step 3. dataset')
-
+    #dataset = MVTecDRAEMTrainDataset(args.data_path + obj_name + "/train/good/",
+    #                                         args.anomaly_source_path,
+    #                                     resize_shape=[256, 256])
+    #dataloader = DataLoader(dataset, batch_size=args.bs,shuffle=True, num_workers=16)
 
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Anomal Lora')
-    parser.add_argument('--log_path', type=str)
+    parser.add_argument('--output_dir', type=str, default='output')
     parser.add_argument('--pretrained_model_name_or_path', type=str,
                         default='facebook/diffusion-dalle')
     args = parser.parse_args()
