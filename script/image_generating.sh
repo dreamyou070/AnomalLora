9843916496bf557f11ed8bfeeb8456f5066f4596
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-04 14:45:49 +0900

    image generating

--- script/image_generating.sh
+++ script/image_generating.sh
@@ -3,7 +3,7 @@
 port_number=53102
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
- --main_process_port $port_number ../train.py \
+ --main_process_port $port_number ../image_generating.py \
  --output_dir ../../result/20240204 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --train_unet \
