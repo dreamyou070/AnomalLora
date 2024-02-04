426a7c6cbab15d776cf05cf02b898e616f606e96
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-05 01:05:15 +0900

    Update image_generating.py

--- image_generating.py
+++ image_generating.py
@@ -114,19 +114,7 @@ def main(args) :
                                guidance_scale=args.guidance_scale,
                                negative_prompt=args.negative_prompt,)
             recon_image = pipeline.latents_to_image(latents[-1])[0].resize((512,512))
-            recon_image.save(f'before_controller_test.png')
-
-            controller = AttentionStore()
-            register_attention_control(unet, controller)
-            latents = pipeline(prompt=gen_caption,
-                               height=512, width=512,
-                               num_inference_steps=args.num_ddim_steps,
-                               guidance_scale=args.guidance_scale,
-                               negative_prompt=args.negative_prompt, )
-            recon_image = pipeline.latents_to_image(latents[-1])[0].resize((512, 512))
-            recon_image.save(f'after_controller_test.png')
-            import time
-            time.sleep(1000)
+            recon_image.save(os.path.join(adjective_base_folder, f'anomal_{i}.png'))
 
 
 
