8f1c2fc3b557fd6448a0f930c89463014953df7f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-04 12:21:24 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,4 +1,4 @@
 # !/bin/bash
 
 python ../train.py \
- --pretrained_model_name_or_path 
+ --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors
