8366340d3a420fa4ce1e50366f9cf55d9d4f5074
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-05 01:52:17 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -6,8 +6,8 @@ obj_name='bagel'
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/caption_bagel_res_64_train_whole_anomal_normal_1_attn_loss_1/models" \
+ --network_folder "../../result/${obj_name}/caption_bagel_res_64_train_whole_anomal_normal_1_attn_loss_0.1_general_training/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
- --prompt 'bagel'
+ --prompt 'good'
