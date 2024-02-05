8816cbec882d2cbd241dc3434381ad7ace247037
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-05 17:59:07 +0900

    Update attention_control.py

--- utils/attention_control.py
+++ utils/attention_control.py
@@ -51,7 +51,8 @@ def register_attention_control(unet: nn.Module,controller: AttentionStore, ):  #
                         noise = torch.randn_like(query).to(hidden_states.device)
                     anomal_map, anomal_features = [], []
                     for pix_idx in range(pix_num):
-                        sub_feature, normal_feat = noise[pix_idx].squeeze(0), normal_query[pix_idx].squeeze(0)
+                        sub_feature, normal_feat = noise[pix_idx, :].squeeze(0), normal_query[pix_idx, :].squeeze(0)
+                        print(f'sub_feature : {sub_feature.shape}, normal_feat : {normal_feat.shape}')
                         sub_dist = mahal(sub_feature.float(), normal_mu, normal_cov)
                         if sub_dist > mean_dist.item():
                             anomal_features.append(sub_feature.unsqueeze(0))
