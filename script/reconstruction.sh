c39d36bedef426e61b08889db0fe44aa7e236d95
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-06 00:58:34 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -7,7 +7,7 @@ caption='good'
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/caption_${caption}_latent_anomal_new_mahal_func_down_dim_100_attn_weight_0.01/models" \
+ --network_folder "../../result/${obj_name}/caption_${caption}_latent_anomal_new_mahal_func_down_dim_100_attn_weight_0.01" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
