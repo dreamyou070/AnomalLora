33ca6a0a9176f895fb703caa3e44f6f426a4cf9f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-05 09:16:40 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,11 +1,12 @@
 # !/bin/bash
-port_number=53888
+port_number=53889
 obj_name='bagel'
+caption='good'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../train.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
- --output_dir "../../result/${obj_name}/caption_bagel_res_64_general_anomal_source_whole_anomal" \
+ --output_dir "../../result/${obj_name}/caption_${caption}_res_64_general_anomal_source_partial_anomal" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path '../../../MyData/anomaly_detection/MVTec3D-AD' \
  --obj_name "${obj_name}" \
@@ -16,5 +17,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 \
  --normal_weight 1 \
- --general_training \
+ --general_training --do_synthetic_anomaly \
  --num_epochs 30
