9e3c4bf423b9c1a4a3d794be5e61b2595b3f6cf1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-05 10:12:41 +0900

    recon

--- script/train.sh
+++ script/train.sh
@@ -16,7 +16,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
  --do_task_loss --task_loss_weight 1.0 \
  --do_dist_loss --dist_loss_weight 1.0 \
- --do_attn_loss --attn_loss_weight 1.0 \
+ --do_attn_loss --attn_loss_weight 0.1 \
  --normal_weight 1 \
  --do_synthetic_anomaly \
  --num_epochs 30
