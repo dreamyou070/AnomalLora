0727f6231ca3c9fdceb3e09e29d9435c7455c194
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-05 19:02:32 +0900

    Update attention_control.py

--- utils/attention_control.py
+++ utils/attention_control.py
@@ -45,6 +45,7 @@ def register_attention_control(unet: nn.Module,controller: AttentionStore, ):  #
                     # ---------------------------------------------------------------------------------------------- #
                     if mask == 'perlin' : # mask means using perlin noise
                         perlin_noise = make_perlin_noise(pix_num, dim)
+                        perlin_noise = hidden_states.squeeze() * perlin_noise
                         noise = torch.tensor(perlin_noise).to(hidden_states.device)
                     else :
                         noise = torch.randn_like(hidden_states).to(hidden_states.device)
