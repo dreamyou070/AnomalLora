79aa8d3244a1c59bc3680d4a8fca50d21de16d94
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-05 21:29:36 +0900

    Update train_latent_anomal.sh

--- script/train_latent_anomal.sh
+++ script/train_latent_anomal.sh
@@ -5,9 +5,9 @@ obj_name='bagel'
 trigger_word='good'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
- --main_process_port $port_number ../train_latent_anomal_attn_0.03.py \
+ --main_process_port $port_number ../train_latent_anomal.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
- --output_dir "../../result/${obj_name}/caption_${trigger_word}_latent_anomal_new_mahal_func_down_dim_100_attn_weight_0.1" \
+ --output_dir "../../result/${obj_name}/caption_${trigger_word}_latent_anomal_new_mahal_func_down_dim_100_attn_weight_0.01" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path '../../../MyData/anomaly_detection/MVTec3D-AD' \
  --obj_name "${obj_name}" \
@@ -17,4 +17,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --trigger_word "${trigger_word}" \
  --do_task_loss --task_loss_weight 1.0 --do_cls_train \
  --do_dist_loss --dist_loss_weight 1.0 \
- --do_attn_loss --attn_loss_weight 0.1 --normal_weight 1
+ --do_attn_loss --attn_loss_weight 0.01 --normal_weight 1
