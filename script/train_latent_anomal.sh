86d62c2199a07b4f75958320c2d59a606a6489c2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-06 13:20:48 +0900

    train latent anomal

--- script/train_latent_anomal.sh
+++ script/train_latent_anomal.sh
@@ -3,7 +3,8 @@
 port_number=58522
 obj_name='bagel'
 trigger_word='good'
-output_dir="../../result/${obj_name}/caption_${trigger_word}_res_64_general_anomal_source_partial_anomal_attnloss_1_anomal_th_max_anomal_sample_normal_loss_down_dim_160"
+
+output_dir="../../result/${obj_name}/caption_${trigger_word}_res_64_attnloss_1_down_dim_240"
 network_weights="${output_dir}/models/epoch-000004.safetensors"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
@@ -21,6 +22,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --normal_weight 1 \
  --do_anomal_sample_normal_loss \
- --down_dim 160
+ --down_dim 240
  #--network_weights ${network_weights} \
  #--start_epoch 4
