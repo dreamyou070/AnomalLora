91a7e8e01f9855fa85163954683e23480164aba7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-06 14:39:26 +0900

    Update train_pixel_anomal.sh

--- script/train_pixel_anomal.sh
+++ script/train_pixel_anomal.sh
@@ -7,13 +7,13 @@ trigger_word='good'
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../train_pixel_anomal.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
- --output_dir "../../result/${obj_name}/caption_${trigger_word}_test" \
+ --output_dir "../../result/${obj_name}/caption_${trigger_word}_res_64_up_attnloss_1_pixel_anomal" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path '../../../MyData/anomaly_detection/MVTec3D-AD' \
  --obj_name "${obj_name}" \
  --train_unet \
  --train_text_encoder \
- --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
+ --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_task_loss --task_loss_weight 1.0 \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 \
