b60cf8fdad66f844d3aed8eb046b5c373c4811f3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-06 13:51:46 +0900

    again

--- data/test.py
+++ data/test.py
@@ -4,4 +4,5 @@ anomal_mask_np = np.array([[1,0,1],
                  [0,1,1],
                  [1,1,0]])
 mask = np.repeat(np.expand_dims(anomal_mask_np, axis=2), 3, axis=2)
-print(mask)
+dtype = mask.dtype
+mask = mask.astype(dtype)
