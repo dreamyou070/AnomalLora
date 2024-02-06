c1b1f594d9c85b821c55bf9824e89062a359b014
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-06 21:34:33 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,15 +1,15 @@
 # !/bin/bash
 
-port_number=59812
+port_number=59813
 obj_name='bagel'
 caption='good'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/caption_good_down_dim_240/models" \
+ --network_folder "../../result/${obj_name}/caption_good_down_dim_320/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
- --down_dim 240 \
+ --down_dim 320 \
  --prompt "${caption}"
