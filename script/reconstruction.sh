0c220c7d0d5b509b57ec6ded097bdea93a3a2b21
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-06 09:58:17 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -7,7 +7,7 @@ caption='good'
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/caption_good_res_64_general_anomal_source_partial_anomal_attnloss_0.1_anomal_th_max/models" \
+ --network_folder "../../result/${obj_name}/caption_good_res_64_general_anomal_source_partial_anomal_attnloss_0.1_anomal_th_max_anomal_sample_normal_loss/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
