7c6d701a0b94b254a8ae1837a845fba9feea7a16
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-07 17:47:26 +0900

    Update train_object_detector.py

--- train_object_detector.py
+++ train_object_detector.py
@@ -256,15 +256,14 @@ def main(args) :
 
             loss += task_loss
             loss_dict['task_loss'] = task_loss.item()
-
+            print(f'loss 1 : {loss}')
             loss += attn_loss
             loss_dict['attn_loss'] = attn_loss.mean().item()
-
+            print(f'loss 2 : {loss}')
             if args.masked_training :
                 loss += masked_loss
                 loss_dict['masked_loss'] = masked_loss.item()
-
-            print(f'loss : {loss}')
+            print(f'loss 3 : {loss}')
 
             current_loss = loss.detach().item()
             if epoch == args.start_epoch :
