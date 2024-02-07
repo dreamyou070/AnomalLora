6e1f87063d9802f5dd784ac62d6dfa4fd59ee953
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-07 09:16:25 +0900

    Update train_latent_anomal.sh

--- script/train_latent_anomal.sh
+++ script/train_latent_anomal.sh
@@ -1,10 +1,10 @@
 # !/bin/bash
 
-port_number=58502
+port_number=58503
 obj_name='bagel'
 trigger_word='bagel'
-output_dir="../../result/${obj_name}/caption_bagel_down_dim_320_more_generalize"
-network_weights="../../result/${obj_name}/caption_bagel_down_dim_320_more_generalize/models//epoch-000011.safetensors"
+output_dir="../../result/${obj_name}/caption_bagel_only_task_loss"
+#network_weights="../../result/${obj_name}/caption_bagel_down_dim_320_more_generalize/models//epoch-000011.safetensors"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number ../train_latent_anomal.py \
@@ -17,12 +17,12 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
  --num_epochs 100 \
  --trigger_word "${trigger_word}" \
- --do_task_loss --task_loss_weight 1.0 --do_cls_train \
- --do_dist_loss --dist_loss_weight 1.0 \
- --do_attn_loss --attn_loss_weight 1.0 --normal_weight 1 \
- --do_anomal_sample_normal_loss \
- --down_dim 320 \
- --start_epoch 11 \
- --num_repeat 2 \
- --more_generalize \
- --network_weights ${network_weights}
+ --do_task_loss --task_loss_weight 1.0 --down_dim 320 --num_repeat 2
+ #--do_dist_loss --dist_loss_weight 1.0 --do_cls_train \
+ #--do_attn_loss --attn_loss_weight 1.0 --normal_weight 1 \
+ #--do_anomal_sample_normal_loss \
+
+ #--start_epoch 11 \
+
+ #--more_generalize \
+ #--network_weights ${network_weights}
