669e0d5d9ffa8643e3e4c7277c57179112a7fa89
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-07 20:54:17 +0900

    Update reconstruction_final.sh

--- script/reconstruction_final.sh
+++ script/reconstruction_final.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
 
-port_number=59442
+port_number=59441
 obj_name='bagel'
 caption='bagel'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_final.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/caption_bagel_pixel_anomal_up_attn_loss_0.008_new_code_repeat_1/models" \
+ --network_folder "../../result/${obj_name}/caption_bagel_pixel_anomal_up_attn_loss_1_new_code_repeat_1/models" \
  --object_detector_weight "../../result/${obj_name}/object_detector/models/epoch-000100.safetensors" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
