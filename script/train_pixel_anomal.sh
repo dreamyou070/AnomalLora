76a9e57db87fa439f5ee9504cbd502a8b87d2b38
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-07 20:02:21 +0900

    Update train_pixel_anomal.sh

--- script/train_pixel_anomal.sh
+++ script/train_pixel_anomal.sh
@@ -4,7 +4,7 @@ port_number=53802
 obj_name='bagel'
 trigger_word='bagel'
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_pixel_anomal.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
  --output_dir "../../result/${obj_name}/caption_bagel_pixel_anomal_up_attn_loss_1_new_code_repeat_1" \
