b093a31b81d80c903589792143c0725c268d5617
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-07 15:21:40 +0900

    Update train_pixel_anomal.sh

--- script/train_pixel_anomal.sh
+++ script/train_pixel_anomal.sh
@@ -1,13 +1,13 @@
 # !/bin/bash
 
-port_number=53801
+port_number=53802
 obj_name='bagel'
 trigger_word='bagel'
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
  --main_process_port $port_number ../train_pixel_anomal.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
- --output_dir "../../result/${obj_name}/caption_bagel_pixel_anomal_up_attn_loss_1_new_code" \
+ --output_dir "../../result/${obj_name}/caption_bagel_pixel_anomal_up_attn_loss_0.008_new_code" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path '../../../MyData/anomaly_detection/MVTec3D-AD' \
  --trigger_word "${trigger_word}" \
@@ -16,5 +16,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_task_loss --task_loss_weight 1.0 \
  --do_dist_loss --dist_loss_weight 1.0 \
- --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
+ --do_attn_loss --attn_loss_weight 0.008 --do_cls_train --normal_weight 1 \
  --num_epochs 100 --start_epoch 0 --num_repeat 20
