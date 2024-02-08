268b1c9c9a9ea5e19984facae9aa1bc884b8b9c8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-08 13:36:53 +0900

    gradient based iamge generation

--- script/gradient_based_image_generation.sh
+++ script/gradient_based_image_generation.sh
@@ -5,9 +5,9 @@ obj_name='bagel'
 caption='good'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
- --main_process_port $port_number ../map_guided_recon.py \
+ --main_process_port $port_number ../gradient_based_image_generation.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/caption_good_res_64_general_anomal_source_partial_anomal_attnloss_0.1_anomal_th_max_anomal_sample_normal_loss_down_dim_160/models" \
+ --network_folder "../../result/${obj_name}/latent_anomal/latent_caption_bagel_down_dim_320_more_generalize/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
  --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
