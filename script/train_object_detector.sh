5141c9f5002f8823101a4bd1f4d4aec6da8c102b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-08 21:56:24 +0900

    change sh script

--- script/train_object_detector.sh
+++ script/train_object_detector.sh
@@ -16,4 +16,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_task_loss --task_loss_weight 1.0 --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --num_epochs 10 --start_epoch 0 --num_repeat 100
+ --num_epochs 10 --start_epoch 0 --num_repeat 100 \
+ --unet_inchannels 4
