e042f58c8f908d1220c5e484f5c9527cfd238ff1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-09 01:31:34 +0900

    Update reconstruction_final.sh

--- script/reconstruction_final.sh
+++ script/reconstruction_final.sh
@@ -1,16 +1,16 @@
 # !/bin/bash
 
-port_number=59226
+port_number=59223
 obj_name='bagel'
 caption='bagel'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_final.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/caption_bagel_1_without_taskloss_backtoken_separating/models" \
+ --network_folder "../../result/${obj_name}/caption_bagel_1_not_concern_background_without_taskloss_dist_loss_normal_background_with_normal/models" \
  --object_detector_weight "../../result/${obj_name}/object_detector/object_detector_1/models/epoch-000100.safetensors" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --down_dim 320 \
- --prompt "${caption}" --back_token_separating
+ --prompt "${caption}"
