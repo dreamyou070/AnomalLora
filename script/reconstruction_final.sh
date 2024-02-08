7867076bdad5d8889c96d608a68250a62aa58826
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-08 14:12:03 +0900

    Update reconstruction_final.sh

--- script/reconstruction_final.sh
+++ script/reconstruction_final.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=59302
+port_number=59111
 obj_name='bagel'
 caption='bagel'
 
@@ -11,6 +11,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --object_detector_weight "../../result/${obj_name}/object_detector/models/epoch-000100.safetensors" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
- --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
+ --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --down_dim 320 \
  --prompt "${caption}"
