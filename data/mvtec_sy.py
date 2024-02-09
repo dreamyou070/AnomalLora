1cdbc57fb135736decd4a7bf3858763926811b30
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-09 23:53:35 +0900

    Update mvtec_sy.py

--- data/mvtec_sy.py
+++ data/mvtec_sy.py
@@ -145,7 +145,7 @@ class MVTecDRAEMTrainDataset(Dataset):
 
         # idx = torch.randint(0, len(self.image_paths), (1,)).item()
         # anomaly_source_idx = torch.randint(0, len(self.anomaly_source_paths), (1,)).item()
-        if len(self.anomaly_source_paths) > 0
+        if len(self.anomaly_source_paths) > 0 :
             anomaly_source_idx = torch.randint(0, len(self.anomaly_source_paths), (1,)).item()
         img_idx = idx % len(self.image_paths)
 
