ba058986d2b1335c9bd1acb7bf34122746d930fb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-10 08:29:54 +0900

    Update train_task_loss_instead_query_optim.sh

--- script/train_task_loss_instead_query_optim.sh
+++ script/train_task_loss_instead_query_optim.sh
@@ -6,7 +6,7 @@ obj_name='bagel'
 trigger_word='bagel'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
- --main_process_port $port_number ../train_pixel_anomal.py \
+ --main_process_port $port_number ../train_task_loss_instead_query_optim.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
  --output_dir "../../result/${obj_name}/train_query_optimizing_from_first" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
