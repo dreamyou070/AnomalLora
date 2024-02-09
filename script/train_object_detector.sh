2fa72f0fb2a462b9b2266165aec793560ecf0057
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-09 17:15:58 +0900

    d

--- script/train_object_detector.sh
+++ script/train_object_detector.sh
@@ -16,5 +16,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_task_loss --task_loss_weight 1.0 --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --num_epochs 30 --start_epoch 0 --num_repeat 1 \
+ --max_train_epochs 30 --start_epoch 0 --num_repeat 1 \
  --unet_inchannels 4
+
+ #  --dist_loss_weight 1.0 --do_cls_train --num_epochs 30
--- train_object_detector.py
+++ train_object_detector.py
@@ -465,10 +465,17 @@ if __name__ == "__main__":
     parser.add_argument("--truncate_pad", action='store_true')
     parser.add_argument("--truncate_length", type=int, default=3)
     parser.add_argument("--anormal_sample_normal_loss", action='store_true')
+
     parser.add_argument("--do_task_loss", action='store_true')
+    parser.add_argument("--task_loss_weight", type=float, default=1.0)
+
     parser.add_argument("--do_dist_loss", action='store_true')
+    parser.add_argument("--dist_loss_weight", type=float, default=1.0)
+
     parser.add_argument("--do_attn_loss", action='store_true')
     parser.add_argument("--attn_loss_weight", type=float, default=1.0)
+    parser.add_argument("--do_cls_train", action='store_true')
+
     parser.add_argument('--normal_weight', type=float, default=1.0)
     parser.add_argument("--clip_skip", type=int, default=None,
                         help="use output of nth layer from back of text encoder (n>=1)")
