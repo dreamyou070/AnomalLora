b997613c783af31ced16d0d901d986833897dfe8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-09 21:11:56 +0900

    1 again

--- script/train_pixel_anomal.sh
+++ script/train_pixel_anomal.sh
@@ -8,7 +8,7 @@ trigger_word='bagel'
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train_pixel_anomal.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
- --output_dir "../../result/${obj_name}/3_caption_bagel_1_without_taskloss_dist_loss_background_with_normal_more_anomal_srcs_anomal_detail" \
+ --output_dir "../../result/${obj_name}/1_caption_bagel_1_not_concern_background_without_taskloss_more_anomal_srcs_srcs_anomal_detail" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path '../../../MyData/anomaly_detection/MVTec3D-AD' \
  --trigger_word "${trigger_word}" \
@@ -18,4 +18,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --background_with_normal --max_train_epochs 300 --start_epoch 0 --num_repeat 1 --anomal_only_on_object --unet_inchannels 4
+ --max_train_epochs 300 --start_epoch 0 --num_repeat 1 --anomal_only_on_object --unet_inchannels 4
