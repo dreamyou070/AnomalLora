d0b5a2d32224e2b0aae7872572d046dfae21d4a5
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-11 08:00:09 +0900

    Update train_pixel_anomal.sh

--- script/train_pixel_anomal.sh
+++ script/train_pixel_anomal.sh
@@ -16,7 +16,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --start_epoch 14 \
+ --start_epoch 0 \
  --max_train_epochs 300 \
  --num_repeat 1 \
  --anomal_only_on_object \
