15e799971f6a182b44f6fbd1c2c0fe11ca09e533
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-10 14:53:17 +0900

    Update with_two_timestep.sh

--- script/with_two_timestep.sh
+++ script/with_two_timestep.sh
@@ -17,4 +17,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --background_with_normal --num_epochs 300 --start_epoch 0 --num_repeat 1 --anomal_only_on_object --unet_inchannels 4
+ --background_with_normal --start_epoch 0 --num_repeat 1 --anomal_only_on_object --unet_inchannels 4
