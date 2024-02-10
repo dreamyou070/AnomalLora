35869a267bc9b33f505a14cb197c04bf43c2d5b9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-10 18:09:55 +0900

    Update mvtec_sy.py

--- data/mvtec_sy.py
+++ data/mvtec_sy.py
@@ -196,15 +196,16 @@ class MVTecDRAEMTrainDataset(Dataset):
 
                     p = random.random()
 
-                    if p > 0 :  # original noise
+                    #if p > 0 :  # original noise
 
-                        while True:
-                            anomal_mask_np, anomal_mask_pil = self.make_random_mask(self.resize_shape[0],
-                                                                                    self.resize_shape[1])
-                            anomal_mask_np = np.where(anomal_mask_np == 0, 0, 1)  # strict anomal (0, 1
-                            anomal_mask_np = anomal_mask_np * object_mask_np
-                            if anomal_mask_np.sum() > 0:
-                                break
+                    while True:
+                        anomal_mask_np, anomal_mask_pil = self.make_random_mask(self.resize_shape[0],
+                                                                                self.resize_shape[1])
+                        anomal_mask_np = np.where(anomal_mask_np == 0, 0, 1)  # strict anomal (0, 1
+                        anomal_mask_np = anomal_mask_np * object_mask_np
+                        if anomal_mask_np.sum() > 0:
+                            break
+                    """
                     else:
 
                         h, w = object_mask_latent_np.shape
@@ -223,7 +224,7 @@ class MVTecDRAEMTrainDataset(Dataset):
                         radius_p = random.triangular(0.01, 0.05)
                         radius = int(radius_p * self.resize_shape[0])
                         anomal_mask_np = cv2.circle(random_mask, center, radius, (1, 1, 1), -1)
-
+                    """
                 #print(f'anomal_mask_np.sum() : {anomal_mask_np.sum()}')
                 mask = np.repeat(np.expand_dims(anomal_mask_np, axis=2), 3, axis=2).astype(dtype)
                 anomal_img = (1 - mask) * img + mask * anomal_src
