c1fd84b63bf57fb899300f788c174f94176f4af3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-10 13:13:37 +0900

    1

--- script/zero_out_noise.sh
+++ script/zero_out_noise.sh
@@ -1,14 +1,14 @@
 # !/bin/bash
 # if backtoken separating, CLS / Trigger / Background
 
-port_number=59173
+port_number=59101
 obj_name='bagel'
 trigger_word='bagel'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../zero_out_noise.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
- --output_dir "../../result/${obj_name}/3_caption_bagel_64_up_zero_out_pseudo_anomal_with_init_noise" \
+ --output_dir "../../result/${obj_name}/3_caption_bagel_64_up_1_pixel_anomal_normal_without_init_noise" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path '../../../MyData/anomaly_detection/MVTec3D-AD' \
  --trigger_word "${trigger_word}" \
@@ -19,4 +19,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
  --max_train_epochs 300 --start_epoch 0 --num_repeat 1 --anomal_only_on_object --unet_inchannels 4 \
- --background_with_normal --truncating --pseudo_anomal_with_init_noise
+ --background_with_normal --truncating \
+ --with_pixel_anomal --normal_without_init_noise
--- zero_out_noise.py
+++ zero_out_noise.py
@@ -189,10 +189,14 @@ def main(args):
                 encoder_hidden_states = enc_out["last_hidden_state"]
 
             with torch.no_grad():
-                anomal_latents = vae.encode(batch['augmented_image'].to(dtype=weight_dtype)).latent_dist.sample()
-                anomal_latents = anomal_latents * vae_scale_factor
+                if args.with_pixel_anomal :
+                    latents = vae.encode(batch['augmented_image'].to(dtype=weight_dtype)).latent_dist.sample()
+                    latents = latents * vae_scale_factor
+                else :
+                    latents = vae.encode(batch['image'].to(dtype=weight_dtype)).latent_dist.sample()
+                    latents = latents * vae_scale_factor
 
-            noise, anomal_noisy_latents, timesteps = get_noise_noisy_latents_and_timesteps(args, noise_scheduler,anomal_latents)
+            noise, noisy_latents, timesteps = get_noise_noisy_latents_and_timesteps(args, noise_scheduler,latents)
             anomal_mask_ = batch['anomaly_mask'].squeeze()  # [64,64]
             res = anomal_mask_.shape[0]
             anomal_mask = anomal_mask_.flatten().unsqueeze(0)
@@ -200,10 +204,10 @@ def main(args):
             anomal_mask = anomal_mask.repeat(1, 4, 1, 1).to(accelerator.device)
 
             """ psuedo anomal = not with init noise, normal = with init noise """
-            anomal_noisy_latents = (1-anomal_mask) * anomal_noisy_latents + (anomal_mask) * anomal_latents
-
-            if args.pseudo_anomal_with_init_noise :
-                anomal_noisy_latents = (1-anomal_mask) * anomal_noisy_latents + (anomal_mask) * anomal_latents
+            if args.normal_without_init_noise :
+                anomal_noisy_latents = (anomal_mask) * noisy_latents + (1-anomal_mask) * latents
+            else :
+                anomal_noisy_latents = (1-anomal_mask) * noisy_latents + (anomal_mask) * latents
 
             with accelerator.autocast():
                 unet(anomal_noisy_latents, timesteps, encoder_hidden_states, trg_layer_list=args.trg_layer_list,noise_type=None).sample
@@ -517,7 +521,8 @@ if __name__ == "__main__":
     parser.add_argument("--noise_type", type=str)
     parser.add_argument("--only_zero_timestep", action="store_true")
     parser.add_argument("--truncating", action="store_true")
-    parser.add_argument("--pseudo_anomal_with_init_noise", action="store_true")
+    parser.add_argument("--with_pixel_anomal", action="store_true")
+    parser.add_argument("--normal_without_init_noise", action="store_true")
     args = parser.parse_args()
     from model.unet import unet_passing_argument
     from utils.attention_control import passing_argument
