715796777ebcfc2f39f34bb7d47dc62627893acb
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-10 10:21:07 +0900

    Update reconstruction_final.sh

--- script/reconstruction_final.sh
+++ script/reconstruction_final.sh
@@ -1,17 +1,17 @@
 # !/bin/bash
 
-port_number=59216
+port_number=59227
 obj_name='bagel'
 caption='bagel'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_final.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/6_caption_bagel_64_down/models" \
+ --network_folder "../../result/${obj_name}/query_permute_64_down_1/models" \
  --object_detector_weight "../../result/${obj_name}/object_detector_experiments/object_detector_1/models/epoch-000100.safetensors" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
- --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
+ --trg_layer_list "['down_blocks_0_atten27ons_1_transformer_blocks_0_attn2']" \
  --network_dim 64 --network_alpha 4 \
  --down_dim 320 \
  --unet_inchannels 4 \
