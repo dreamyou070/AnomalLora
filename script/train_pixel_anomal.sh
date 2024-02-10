850f9dce22bbd2f3640cb1f84403091a35c2f540
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-10 20:13:42 +0900

    a

--- script/train_pixel_anomal.sh
+++ script/train_pixel_anomal.sh
@@ -13,7 +13,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --trigger_word "${trigger_word}" \
  --obj_name "${obj_name}" \
  --train_unet --train_text_encoder \
- --network_weights "../../result/${obj_name}/1_64_down/models/epoch-000014.safetensors" \
+ --network_weights "../../result/${obj_name}/2_64_up_1/models/epoch-000014.safetensors" \
  --trg_layer_list "['up_blocks_3_attentions_1_transformer_blocks_0_attn2']" \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
