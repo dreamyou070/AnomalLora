b6ef8f0c175fad1dc9355bc3c6d5cd9ad04b9998
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-11 12:16:48 +0900

    Update reconstruction_final.sh

--- script/reconstruction_final.sh
+++ script/reconstruction_final.sh
@@ -1,16 +1,16 @@
 # !/bin/bash
 
-port_number=59986
+port_number=58686
 obj_name='cable_gland'
 caption='cable'
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_final.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/64_up_2_total_normal_thred_0.1/models" \
+ --network_folder "../../result/${obj_name}/64_down_1_total_normal_thred_1.0_anomal_src_more_perlin_max_scale_10_kernel_size_5/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
- --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
+ --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
  --network_dim 64 --network_alpha 4 \
  --down_dim 320 \
  --unet_inchannels 4 \
