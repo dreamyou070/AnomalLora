d075f1fc4ef1050a1bac3e06634983fd6576fff9
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-11 11:49:50 +0900

    Update reconstruction_final.sh

--- script/reconstruction_final.sh
+++ script/reconstruction_final.sh
@@ -7,7 +7,7 @@ caption='cable'
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_final.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/64_up_2_total_normal_thred_1.0_anomal_src_more/models" \
+ --network_folder "../../result/${obj_name}/64_up_2_total_normal_thred_0.1/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
