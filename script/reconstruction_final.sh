00b7864a9be078119dd46d2398a0de2db0d80f33
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-13 09:35:29 +0900

    recon

--- script/reconstruction_final.sh
+++ script/reconstruction_final.sh
@@ -7,11 +7,8 @@ caption='cable'
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_final.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/up_2_basic/models" \
+ --network_folder "../../result/${obj_name}/down_1_basic/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" \
- --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
- --network_dim 64 --network_alpha 4 \
- --down_dim 320 \
- --unet_inchannels 4 \
- --prompt "${caption}" --truncating
+ --trg_layer_list "['down_blocks_0_attentions_1_transformer_blocks_0_attn2']" \
+ --network_dim 64 --network_alpha 4 --unet_inchannels 4 --prompt "${caption}" --truncating
