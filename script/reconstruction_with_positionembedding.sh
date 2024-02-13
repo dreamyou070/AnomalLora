3f370651e5f6749afdd27bcf48ce9763aa37f08b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 08:23:24 +0900

    Update reconstruction_with_positionembedding.sh

--- script/reconstruction_with_positionembedding.sh
+++ script/reconstruction_with_positionembedding.sh
@@ -1,11 +1,11 @@
 # !/bin/bash
-#
 
-port_number=51041
+port_number=51042
 obj_name='cable_gland'
 caption='cable_gland'
 # --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1'
-#
+# --do_local_self_attn --fixed_window_size --window_size 8 \
+
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_with_positionembedding.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
@@ -14,4 +14,7 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --obj_name "${obj_name}" --network_dim 64 --network_alpha 4 --unet_inchannels 4 --prompt "${caption}" --latent_res 64 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --d_dim 320 --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
- --do_local_self_attn --fixed_window_size --window_size 8
+ --add_query_layer_list '["down_blocks_0_attentions_0_transformer_blocks_0_attn2",
+                          "down_blocks_0_attentions_1_transformer_blocks_0_attn2",
+                          "up_blocks_3_attentions_0_transformer_blocks_0_attn2",
+                          "up_blocks_3_attentions_1_transformer_blocks_0_attn2"]'
