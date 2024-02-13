d6627e26af1b233f5f5205dc0903f1976c404c5b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-13 23:22:08 +0900

    Update reconstruction_with_positionembedding.sh

--- script/reconstruction_with_positionembedding.sh
+++ script/reconstruction_with_positionembedding.sh
@@ -12,6 +12,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --network_folder "../../result/${obj_name}/up_2_basic_pe_beta_scale_0.8_rand_aug_data/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
- --obj_name "${obj_name}" --network_dim 128 --network_alpha 4 --unet_inchannels 4 --prompt "${caption}" --latent_res 64 \
+ --obj_name "${obj_name}" --network_dim 64 --network_alpha 4 --unet_inchannels 4 --prompt "${caption}" --latent_res 64 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --d_dim 320 --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1'
