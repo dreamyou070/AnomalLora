8348ca3c4cb51bd32d84f3c382979013a07c5683
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-13 23:32:27 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -3,7 +3,7 @@
 class_name="cable_gland"
 dataset_cat="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
-sub_folder="up_2_basic_without_total_normal_pe_beta_scale_0.6"
+sub_folder="up_2_basic_pe_beta_scale_0.8_rand_aug_data"
 
 
 output_dir="metrics"
