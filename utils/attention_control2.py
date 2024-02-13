a894a7a5c157a7650f0a7e95b3e752b35234371a
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 00:55:35 +0900

    global and local attn

--- utils/attention_control2.py
+++ utils/attention_control2.py
@@ -127,11 +127,12 @@ def register_attention_control(unet: nn.Module,controller: AttentionStore):
             attention_scores = torch.baddbmm(torch.empty(query.shape[0], query.shape[1], key.shape[1], dtype=query.dtype, device=query.device), query,
                                              key.transpose(-1, -2), beta=0, alpha=self.scale, )
             attention_probs = attention_scores.softmax(dim=-1).to(value.dtype)
-            hidden_states = torch.bmm(attention_probs, value)
-            hidden_states = self.reshape_batch_dim_to_heads(hidden_states)
-            hidden_states = self.to_out[0](hidden_states)
-            local_hidden_states_out = torch.zeros_like(hidden_states)
+            global_hidden_states = torch.bmm(attention_probs, value)
 
+
+
+
+            local_hidden_states = torch.zeros_like(global_hidden_states)
             if not is_cross_attention and do_local_self_attn :
                 for window_index in range(window_num):
                     l_query = local_query[window_index * self.heads : (window_index + 1) * self.heads, :, :]
@@ -144,10 +145,15 @@ def register_attention_control(unet: nn.Module,controller: AttentionStore):
                     l_attention_scores = torch.baddbmm(torch.empty(l_query.shape[0], l_query.shape[1], l_key.shape[1], dtype=l_query.dtype, device=l_query.device),
                                                        l_query, l_key.transpose(-1, -2),
                                                        beta=0, alpha=self.scale, ).softmax(dim=-1).to(l_value.dtype)
-                    l_hidden_states = self.reshape_batch_dim_to_heads(torch.bmm(l_attention_scores, l_value))
-                    l_hidden_states = self.to_out[0](l_hidden_states)
+                    l_hidden_states = torch.bmm(l_attention_scores, l_value)
                     local_pix_num = l_hidden_states.shape[1]
-                    local_hidden_states_out[:, window_index * local_pix_num: (window_index + 1) * local_pix_num, :] = l_hidden_states
+                    local_hidden_states[:, window_index * local_pix_num: (window_index + 1) * local_pix_num, :] = l_hidden_states
+                    # -------------------------------------------------------------------------------------------------------- #
+
+                    #l_hidden_states = self.reshape_batch_dim_to_heads(local_hidden_states)
+                    #l_hidden_states = self.to_out[0](l_hidden_states)
+                    #local_pix_num = l_hidden_states.shape[1]
+                    #local_hidden_states_out[:, window_index * local_pix_num: (window_index + 1) * local_pix_num, :] = l_hidden_states
 
             if trg_layer_list is not None and layer_name in trg_layer_list :
                 trg_map = attention_probs[:, :, :2]
@@ -155,9 +161,18 @@ def register_attention_control(unet: nn.Module,controller: AttentionStore):
 
             if not is_cross_attention and do_local_self_attn :
                 if only_local_self_attn :
-                    hidden_states = local_hidden_states_out
+                    hidden_states = self.reshape_batch_dim_to_heads(local_hidden_states)
+                    hidden_states = self.to_out[0](hidden_states)
+
                 else :
-                    hidden_states = hidden_states + local_hidden_states_out
+                    total_hidden_states = global_hidden_states + local_hidden_states
+                    hidden_states = self.reshape_batch_dim_to_heads(total_hidden_states)
+                    hidden_states = self.to_out[0](hidden_states)
+            else :
+                # -------------------------------------------------------------------------------------------------------- #
+                hidden_states = self.reshape_batch_dim_to_heads(global_hidden_states)
+                hidden_states = self.to_out[0](hidden_states)
+
             return hidden_states
 
         return forward
