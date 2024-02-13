7907dab99acd9ca7853e9f8fa1abe6e0333ba570
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-13 23:40:17 +0900

    Update reconstruction_with_positionembedding.sh

--- script/reconstruction_with_positionembedding.sh
+++ script/reconstruction_with_positionembedding.sh
@@ -1,16 +1,16 @@
 # !/bin/bash
 #
 
-port_number=51023
-obj_name='carrot'
-caption='carrot'
+port_number=51033
+obj_name='cable_gland'
+caption='cable_gland'
 # --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1'
 #
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_with_positionembedding.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/up_2_basic_pe_beta_scale_0.8/models" \
+ --network_folder "../../result/${obj_name}/up_2_basic_pe_beta_scale_0.8_window_size_8/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" --network_dim 64 --network_alpha 4 --unet_inchannels 4 --prompt "${caption}" --latent_res 64 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
