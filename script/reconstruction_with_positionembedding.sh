05281a2a5f9381f3fcca9919c0fe7270b520207f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 08:16:33 +0900

    Update reconstruction_with_positionembedding.sh

--- script/reconstruction_with_positionembedding.sh
+++ script/reconstruction_with_positionembedding.sh
@@ -6,7 +6,6 @@ obj_name='cable_gland'
 caption='cable_gland'
 # --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1'
 #
-
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction_with_positionembedding.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
