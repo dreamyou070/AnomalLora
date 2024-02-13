cdb6ef54c89d586f6ee40320672a0bbbeb759581
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-13 22:41:07 +0900

    Update train_with_positionembedding.sh

--- script/train_with_positionembedding.sh
+++ script/train_with_positionembedding.sh
@@ -3,7 +3,7 @@ port_number=50077
 obj_name='cable_gland'
 trigger_word='cable'
 
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_config \
  --main_process_port $port_number ../train_with_positionembedding.py \
  --log_with wandb --wandb_api_key 3a3bc2f629692fa154b9274a5bbe5881d47245dc \
  --output_dir "../../result/${obj_name}/up_2_basic_pe_beta_scale_0.8_do_add_query" \
