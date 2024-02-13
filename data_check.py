a20a2162f623d62f016cad92c57795104fd3777b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-13 10:18:39 +0900

    Update data_check.py

--- data_check.py
+++ data_check.py
@@ -14,6 +14,8 @@ def main(args):
     print(f'\n step 2. dataset')
     obj_name = args.obj_name
     root_dir = f'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/{obj_name}/train_1/good/rgb'
+    num_images = len(os.listdir(root_dir))
+    print(f'num_images: {num_images}')
     args.anomaly_source_path = f'/home/dreamyou070/MyData/anomaly_detection/MVTec3D-AD/anomal_source_{obj_name}'
     tokenizer = load_tokenizer(args)
     dataset = MVTecDRAEMTrainDataset(root_dir=root_dir,
