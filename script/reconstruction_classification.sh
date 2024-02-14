3eeccc0dfa0ef69575cf17e8cfd2edc5651296c4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 23:12:39 +0900

    Update reconstruction_classification.sh

--- script/reconstruction_classification.sh
+++ script/reconstruction_classification.sh
@@ -11,4 +11,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" --network_dim 64 --network_alpha 4 --unet_inchannels 4 --prompt "${caption}" --latent_res 64 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
- --d_dim 320 --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1'
+ --d_dim 320 --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
+ --image_classification_layer "mid_block_attentions_0_transformer_blocks_0_attn2"
