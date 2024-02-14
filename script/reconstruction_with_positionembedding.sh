25eab9bcfdf7964aade96b61340f99d214cae0a8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 14:41:34 +0900

    Update reconstruction_with_positionembedding.sh

--- script/reconstruction_with_positionembedding.sh
+++ script/reconstruction_with_positionembedding.sh
@@ -7,9 +7,9 @@ caption='cable'
 # --do_local_self_attn --fixed_window_size --window_size 8 \
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
- --main_process_port $port_number ../up_2_pe_beta_scale_0.8_normal_map_loss.py \
+ --main_process_port $port_number ../reconstruction_with_positionembedding.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/up_2_basic_pe_beta_scale_0.8/models" \
+ --network_folder "../../result/${obj_name}/up_2_pe_beta_scale_0.8_normal_map_loss/models" \
  --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
  --obj_name "${obj_name}" --network_dim 64 --network_alpha 4 --unet_inchannels 4 --prompt "${caption}" --latent_res 64 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
