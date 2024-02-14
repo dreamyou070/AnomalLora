ced8a7bbdfa918ad2414d3c80125c42eaeb7e7b2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-15 00:51:58 +0900

    Update train_with_positionembedding.sh

--- script/train_with_positionembedding.sh
+++ script/train_with_positionembedding.sh
@@ -1,7 +1,7 @@
 # !/bin/bash
 
 # --do_classification --image_classification_layer "up_blocks_3_attentions_2_transformer_blocks_0_attn2"
-port_number=55454
+port_number=54545
 
 obj_name='carrot'
 trigger_word='carrot'
@@ -9,9 +9,10 @@ trigger_word='carrot'
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --main_process_port $port_number ../train_with_positionembedding.py \
  --log_with wandb \
- --output_dir "../../result/${obj_name}/up_2_basic_change_dataset_class" \
+ --output_dir "../../result/${obj_name}/up_2_basic_change_dataset_class_small_anomal" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path '../../../MyData/anomaly_detection/MVTec3D-AD' --beta_scale_factor 0.8 \
+ --use_small_anomal \
  --use_position_embedder --d_dim 320 --latent_res 64 --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" --train_unet --train_text_encoder \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
