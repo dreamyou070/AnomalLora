961701b961b3b0827537d3092835a47bd9fb1390
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 21:21:23 +0900

    Update train_with_positionembedding.sh

--- script/train_with_positionembedding.sh
+++ script/train_with_positionembedding.sh
@@ -1,4 +1,5 @@
 # !/bin/bash
+
 port_number=51544
 
 obj_name='cable_gland'
@@ -17,4 +18,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
  --do_map_loss \
- --do_classification --image_classification_layer "up_blocks_3_attentions_2_transformer_blocks_0_attn2" 
+ --do_classification --image_classification_layer "up_blocks_3_attentions_2_transformer_blocks_0_attn2"
