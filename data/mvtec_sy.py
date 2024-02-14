459ce1c5e7aa39dcaaaefdd76cb8541f76a0cb73
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-14 13:19:11 +0900

    Update mvtec_sy.py

--- data/mvtec_sy.py
+++ data/mvtec_sy.py
@@ -176,7 +176,7 @@ class MVTecDRAEMTrainDataset(Dataset):
             y_0 = torch.randint(int(end_num / 4), int(3 * end_num / 4), (1,)).item()
             sigma = torch.randint(25, 60, (1,)).item()
             result = np.exp(-4 * np.log(2) * ((x - x_0) ** 2 + (y - y_0) ** 2) / sigma ** 2)  # 0 ~ 1
-            result = np.where(result < 0.5, 0, result)
+            result = np.where(result < 0.5, 0, 1)
             # only on object
             result_thr = cv2.GaussianBlur((result * object_position), (1,1), 0)
             if np.sum(result_thr) > 0:
