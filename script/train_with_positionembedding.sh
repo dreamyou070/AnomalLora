a26d0d4c922070ba13013ac4395ea51d490a19f2
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-15 18:55:35 +0900

    Update train_with_positionembedding.sh

--- script/train_with_positionembedding.sh
+++ script/train_with_positionembedding.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=50004
+port_number=50005
 
 obj_name='carrot'
 trigger_word='carrot'
@@ -9,11 +9,11 @@ bench_mark='MVTec3D-AD'
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --main_process_port $port_number ../train_with_positionembedding.py \
  --log_with wandb \
- --output_dir "../../result/${obj_name}/up_2_not_anomal_hole_act_deact_do_down_dim_mahal_loss_map_loss_with_focal_loss" \
+ --output_dir "../../result/${obj_name}/up_2_not_anomal_hole_act_deact_do_down_dim_mahal_loss_map_loss_with_focal_loss_on_unet" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" --train_unet --train_text_encoder \
- --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
+ --use_position_embedder --position_embedding_layer 'unet' \
  --d_dim 320 --latent_res 64 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 300 --anomal_only_on_object --unet_inchannels 4 --min_timestep 0 --max_timestep 1000 \
