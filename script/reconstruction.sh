676021f167dc00756a5e49fda2053f2cc0187cc3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-15 20:35:27 +0900

    recon 2

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -3,13 +3,14 @@
 port_number=53204
 obj_name='carrot'
 caption='carrot'
+folder_name="up_2_not_anomal_hole_act_deact_do_down_dim_mahal_loss_map_loss_with_focal_loss"
+bench_mark="MVTec3D-AD"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_folder "../../result/${obj_name}/up_2_not_anomal_hole_act_deact_do_down_dim_mahal_loss_map_loss_with_focal_loss/models" \
- --data_path "../../../MyData/anomaly_detection/MVTec3D-AD/${obj_name}/test" \
- --obj_name "${obj_name}" --network_dim 64 --network_alpha 4 \
- --prompt "${caption}" --latent_res 64 \
- --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
+ --network_dim 64 --network_alpha 4 --network_folder "../../result/${obj_name}/${folder_name}/models" \
+ --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
+ --obj_name "${obj_name}" --prompt "${caption}" \
+ --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --d_dim 320 --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1'
