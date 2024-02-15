5480a3ba2548f7628d18fbec7078c647bd46681d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-15 12:49:58 +0900

    Update train_with_positionembedding.sh

--- script/train_with_positionembedding.sh
+++ script/train_with_positionembedding.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=50001
+port_number=50002
 
 obj_name='carrot'
 trigger_word='carrot'
@@ -8,7 +8,7 @@ trigger_word='carrot'
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --main_process_port $port_number ../train_with_positionembedding.py \
  --log_with wandb \
- --output_dir "../../result/${obj_name}/up_2_not_anomal_hole_act_deact" \
+ --output_dir "../../result/${obj_name}/up_2_not_anomal_hole_act_deact_use_sharpen_aug" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path '../../../MyData/anomaly_detection/MVTec3D-AD' --beta_scale_factor 0.8 \
  --use_position_embedder --d_dim 320 --latent_res 64 --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
@@ -17,4 +17,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --start_epoch 0 --max_train_epochs 300 --anomal_only_on_object --unet_inchannels 4 --min_timestep 0 --max_timestep 1000 \
  --do_dist_loss --dist_loss_weight 1.0 \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --do_map_loss
+ --do_map_loss --use_sharpen_aug
