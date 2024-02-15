a302f3ef9a3946d48586c982d27e980bbe07d059
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-15 18:44:40 +0900

    Update train_with_positionembedding.sh

--- script/train_with_positionembedding.sh
+++ script/train_with_positionembedding.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=50005
+port_number=50004
 
 obj_name='carrot'
 trigger_word='carrot'
@@ -13,7 +13,8 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_config \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" --train_unet --train_text_encoder \
- --use_position_embedder --position_embedding_layer 'unet' --d_dim 320 --latent_res 64 \
+ --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' \
+ --d_dim 320 --latent_res 64 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --start_epoch 0 --max_train_epochs 300 --anomal_only_on_object --unet_inchannels 4 --min_timestep 0 --max_timestep 1000 \
  --do_dist_loss --dist_loss_weight 1.0 \
