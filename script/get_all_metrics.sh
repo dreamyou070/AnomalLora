a09315a25c3718a62e0c7563258d194b9e689c19
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-16 07:52:02 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 class_name="carrot"
-second_folder_name="up_2_not_anomal_hole_act_deact_do_down_dim_mahal_loss"
+second_folder_name="up_2_not_anomal_hole_act_deact_do_down_dim_mahal_loss_map_loss_with_focal_loss_on_unet"
 
 
 python ../evaluation/get_all_metrics.py \
