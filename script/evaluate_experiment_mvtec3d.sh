15b5d0f4c98ec980f7e6c2185e274039ff0a6a45
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-16 08:59:28 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 class_name="carrot"
-dataset_cat="MVTec3D-AD"
-dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
+bench_mark="MVTec3D-AD"
+dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 sub_folder="up_2_not_anomal_hole_act_deact_do_down_dim_mahal_loss"
 
 output_dir="metrics"
 
 
 python ../evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py \
-     --base_dir "../../result/${class_name}/${sub_folder}/reconstruction" \
+     --base_dir "../../result/${bench_mark}/${class_name}/${sub_folder}/reconstruction" \
      --dataset_base_dir "${dataset_dir}" \
      --anomaly_maps_dir "${base_dir}" \
      --output_dir "${output_dir}" \
