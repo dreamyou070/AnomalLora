480e5e1e3767b14a3a5d9bf8118952c06b175b8d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-15 09:26:22 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -3,7 +3,7 @@
 class_name="carrot"
 dataset_cat="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${dataset_cat}"
-sub_folder="up_2_basic_pe_beta_scale_0.8"
+sub_folder="up_2_not_anomal_hole"
 
 
 output_dir="metrics"
