05d85bb113d30a871bc3c14ced4b215e83a2c7e7
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-16 00:34:44 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -3,13 +3,13 @@
 port_number=53266
 obj_name='carrot'
 caption='carrot'
-folder_name="train_student_lora"
+folder_name="up_2_not_anomal_hole_act_deact_do_down_dim_mahal_loss_map_loss_with_focal_loss_on_unet"
 bench_mark="MVTec3D-AD"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --main_process_port $port_number ../reconstruction.py \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
- --network_dim 64 --network_alpha 4 --network_folder "../../result/${bench_mark}/${obj_name}/${folder_name}/models" \
+ --network_dim 64 --network_alpha 4 --network_folder "../../result/${obj_name}/${folder_name}/models" \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
  --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
