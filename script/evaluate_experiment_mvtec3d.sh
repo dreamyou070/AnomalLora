a085333f2476cb3e1744aa2924b9b0e7c58743db
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-16 15:43:33 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -3,13 +3,13 @@
 class_name="carrot"
 bench_mark="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
-sub_folder="6_up_2_anomal_pe_unet"
+sub_folder="6_up_2_anomal_pe_down"
 
 output_dir="metrics"
 
 
 python ../evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py \
-     --base_dir "../../result/${bench_mark}/${class_name}/${sub_folder}/reconstruction" \
+     --base_dir "../../result/${bench_mark}/${class_name}/${sub_folder}/reconstruction_eval" \
      --dataset_base_dir "${dataset_dir}" \
      --anomaly_maps_dir "${base_dir}" \
      --output_dir "${output_dir}" \
