e5f0ce9a71d6592b0cd629314beca76d227ddcd5
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-16 23:46:47 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,18 +1,18 @@
 # !/bin/bash
 
-port_number=50011
+port_number=50009
 
 obj_name='carrot'
 trigger_word='carrot'
 bench_mark='MVTec3D-AD'
 anomal_source_path="../../../MyData/anomal_source"
 #  \
-#
+#  --do_local_self_attn --window_size 8
 # --use_focal_loss
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/11_up_2_anomal_pe_down_down_mahal_task_loss_global_local_self_attn" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/9_up_2_anomal_pe_down_down_mahal_task_loss" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --beta_scale_factor 0.8 --anomal_source_path "${anomal_source_path}" --anomal_only_on_object \
@@ -21,4 +21,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' --d_dim 320 --latent_res 64 \
  --do_dist_loss --dist_loss_weight 1.0 --do_down_dim_mahal_loss --down_dim 100 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --do_map_loss --do_local_self_attn --window_size 8
+ --do_map_loss
