80e79ba7d80449dd00fec0db84ae34295933c8d4
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 07:34:22 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-port_number=51209
+port_number=51210
 obj_name='carrot'
 caption='carrot'
-folder_name="9_up_2_anomal_pe_down_down_mahal_task_loss"
+folder_name="10_up_2_anomal_pe_down_down_mahal_task_loss_only_local_attn"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 
