4ba86135d5ddfd834edc77199bdb4fc0ec896d40
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 08:05:23 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -4,6 +4,10 @@ class_name="carrot"
 bench_mark="MVTec3D-AD"
 dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 sub_folder="9_up_2_anomal_pe_down_down_mahal_task_loss"
+#sub_folder="10_up_2_anomal_pe_down_down_mahal_task_loss_only_local_attn"
+#sub_folder="11_up_2_anomal_pe_down_down_mahal_task_loss_global_local_attn"
+#sub_folder="14_up_2_anomal_pe_down_focal_loss"
+#sub_folder="15_up_2_anomal_pe_down_do_query_shuffle_loss"
 
 output_dir="metrics"
 
@@ -15,5 +19,3 @@ python ../evaluation/evaluation_code_MVTec3D-AD/evaluate_experiment.py \
      --output_dir "${output_dir}" \
      --evaluated_objects "${class_name}" \
      --pro_integration_limit 0.3
-
-     
