9f9d3c7dd51dfd2ea039247502e2c0398e9e4475
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-16 12:32:36 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=50005
+port_number=50007
 
 obj_name='carrot'
 trigger_word='carrot'
@@ -9,7 +9,7 @@ anomal_source_path="../../../MyData/anomal_source"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/6_up_2_anomal_pe_down" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/7_up_2_anomal_pe_down_down_mahal" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --trigger_word "${trigger_word}" --obj_name "${obj_name}" \
  --anomal_source_path ${anomal_source_path} \
@@ -19,4 +19,4 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --start_epoch 0 --max_train_epochs 300 \
  --do_dist_loss --dist_loss_weight 1.0 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
- --do_map_loss
+ --do_map_loss --do_down_dim_mahal_loss
