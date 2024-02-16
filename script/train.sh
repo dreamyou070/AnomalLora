fa6f1728b040e521f962199fe2d649729a3490d3
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-16 11:57:34 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=50002
+port_number=50000
 
 obj_name='carrot'
 trigger_word='carrot'
@@ -10,13 +10,13 @@ anomal_source_path="../../../MyData/anomal_source"
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py \
  --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/1_up_2_anomal_pe_down" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/6_up_2_anomal_pe_unet" \
  --bench_mark "${bench_mark}" \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --beta_scale_factor 0.8 \
  --anomal_source_path "${anomal_source_path}" \
  --trigger_word "${trigger_word}" --obj_name "${obj_name}" --train_unet --train_text_encoder \
- --use_position_embedder --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' --d_dim 320 --latent_res 64  \
+ --use_position_embedder --position_embedding_layer 'unet' --d_dim 320 --latent_res 64  \
  --start_epoch 0 --max_train_epochs 300 \
  --do_dist_loss --dist_loss_weight 1.0 \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
