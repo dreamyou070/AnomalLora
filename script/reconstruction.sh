b62cb10ed8afe3f53d3a49da7556133711c1e444
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 20:12:16 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-port_number=57777
+port_number=50001
 obj_name='carrot'
 caption='carrot'
-folder_name="18_anomal_pe_down_bgtest"
+folder_name="1_train_anomalous_detector_not_small_anomal"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 # --do_local_self_attn --window_size 8
