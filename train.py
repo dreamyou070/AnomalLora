1554d1058913fe71af38249ae8e86651239610c1
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 15:49:10 +0900

    Update train.py

--- train.py
+++ train.py
@@ -95,7 +95,6 @@ def main(args):
                                      perlin_max_scale=args.perlin_max_scale,
                                      kernel_size=args.kernel_size,
                                      beta_scale_factor=args.beta_scale_factor,
-                                     use_sharpen_aug=args.use_sharpen_aug,
                                      do_anomal_hole = args.do_anomal_hole,
                                      bgrm_test = args.bgrm_test)
     train_dataloader = torch.utils.data.DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
