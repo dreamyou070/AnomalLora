cc31d5d02af0af9cf2214bea796caeaf05278756
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 09:39:04 +0900

    Update evaluate_experiment_mvtec3d.sh

--- script/evaluate_experiment_mvtec3d.sh
+++ script/evaluate_experiment_mvtec3d.sh
@@ -6,8 +6,8 @@ dataset_dir="../../../MyData/anomaly_detection/${bench_mark}"
 #sub_folder="9_up_2_anomal_pe_down_down_mahal_task_loss"
 #sub_folder="10_up_2_anomal_pe_down_down_mahal_task_loss_only_local_attn"
 #sub_folder="11_up_2_anomal_pe_down_down_mahal_task_loss_global_local_attn"
-sub_folder="14_up_2_anomal_pe_down_focal_loss"
-#sub_folder="15_up_2_anomal_pe_down_do_query_shuffle_loss"
+#sub_folder="14_up_2_anomal_pe_down_focal_loss"
+sub_folder="15_up_2_anomal_pe_down_do_query_shuffle_loss"
 
 output_dir="metrics"
 
