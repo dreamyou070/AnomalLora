1752d5aa2169d511031ca59c49f15abe71854a4f
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 16:29:58 +0900

    Update train_object_detector.sh

--- script/train_object_detector.sh
+++ script/train_object_detector.sh
@@ -14,6 +14,6 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --beta_scale_factor 0.8 --anomal_source_path "${anomal_source_path}" --anomal_only_on_object --bgrm_test \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}" --trigger_word "${trigger_word}" --obj_name "${obj_name}" --anomal_only_on_object \
- --do_dist_loss --dist_loss_weight 1.0 \
+ --do_dist_loss --dist_loss_weight 1.0 --use_position_embedder \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
  --do_anomal_hole --use_small_anomal
