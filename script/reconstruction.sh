273b808d8d59e9355401a3ea8e15fb18667f2c49
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 10:21:40 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -14,4 +14,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_config \
  --data_path "../../../MyData/anomaly_detection/${bench_mark}/${obj_name}/test" \
  --obj_name "${obj_name}" --prompt "${caption}" \
  --latent_res 64 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
- --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} --do_local_self_attn
+ --d_dim 320 --use_position_embedder --position_embedding_layer ${position_embedding_layer} --do_local_self_attn \
+ --window_size 8
