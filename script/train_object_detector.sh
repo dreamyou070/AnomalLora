7cee67e6100dd1d44b539db175430b680f77ca51
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 16:16:46 +0900

    train object detector

---
+++ script/train_object_detector.sh
@@ -0,0 +1,19 @@
+# !/bin/bash
+
+port_number=50001
+
+obj_name='carrot'
+trigger_word='carrot'
+bench_mark='MVTec3D-AD'
+anomal_source_path="../../../MyData/anomal_source"
+
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
+ --main_process_port $port_number ../train_object_detector.py --log_with wandb \
+ --output_dir "../../result/${bench_mark}/${obj_name}/object_detector" \
+ --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
+ --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
+ --beta_scale_factor 0.8 --anomal_source_path "${anomal_source_path}" --anomal_only_on_object --bgrm_test \
+ --data_path "../../../MyData/anomaly_detection/${bench_mark}" --trigger_word "${trigger_word}" --obj_name "${obj_name}" --anomal_only_on_object \
+ --do_dist_loss --dist_loss_weight 1.0 \
+ --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1 \
+ --do_anomal_hole --use_small_anomal
--- utils/utils_mahalanobis.py
+++ utils/utils_mahalanobis.py
@@ -9,46 +9,25 @@ def gen_mahal_loss(args, anormal_feat_list, normal_feat_list, mu, cov):
 
     normal_feats = torch.cat(normal_feat_list, dim=0)
 
-    if args.previous_mahal :
-        if mu is None:
-            mu = torch.mean(normal_feats, dim=0)
-            cov = torch.cov(normal_feats.transpose(0, 1))
+    mu = torch.mean(normal_feats, dim=0)
+    cov = torch.cov(normal_feats.transpose(0, 1))
 
-        if anormal_feat_list is not None:
-            anormal_feats = torch.cat(anormal_feat_list, dim=0)
-            anormal_mahalanobis_dists = [mahal(feat, mu, cov) for feat in anormal_feats]
-            anormal_dist_mean = torch.tensor(anormal_mahalanobis_dists).mean()
+    if anormal_feat_list is not None:
+        anormal_feats = torch.cat(anormal_feat_list, dim=0)
+        anormal_mahalanobis_dists = [mahal(feat, mu, cov) for feat in anormal_feats]
+        anormal_dist_mean = torch.tensor(anormal_mahalanobis_dists).mean()
 
-        normal_mahalanobis_dists = [mahal(feat, mu, cov) for feat in normal_feats]
-        normal_dist_max = torch.tensor(normal_mahalanobis_dists).max()
+    normal_mahalanobis_dists = [mahal(feat, mu, cov) for feat in normal_feats]
+    normal_dist_max = torch.tensor(normal_mahalanobis_dists).max()
 
-        # [4] loss
-        if anormal_feat_list is not None:
-            total_dist = normal_dist_max + anormal_dist_mean
-        else :
-            total_dist = normal_dist_max
+    # [4] loss
+    if anormal_feat_list is not None:
+        total_dist = normal_dist_max + anormal_dist_mean
         normal_dist_loss = normal_dist_max / total_dist
-        normal_dist_loss = normal_dist_loss * args.dist_loss_weight
-        mu = torch.mean(normal_feats, dim=0)
-        cov = torch.cov(normal_feats.transpose(0, 1))
-    else :
-        mu = torch.mean(normal_feats, dim=0)
-        cov = torch.cov(normal_feats.transpose(0, 1))
-        if anormal_feat_list is not None:
-            anormal_feats = torch.cat(anormal_feat_list, dim=0)
-            anormal_mahalanobis_dists = [mahal(feat, mu, cov) for feat in anormal_feats]
-            anormal_dist_mean = torch.tensor(anormal_mahalanobis_dists).mean()
 
-        normal_mahalanobis_dists = [mahal(feat, mu, cov) for feat in normal_feats]
-        normal_dist_max = torch.tensor(normal_mahalanobis_dists).max()
+    else:
+        normal_dist_loss = normal_dist_max
 
-        # [4] loss
-        if anormal_feat_list is not None:
-            total_dist = normal_dist_max + anormal_dist_mean
-        else:
-            total_dist = normal_dist_max
-        normal_dist_loss = normal_dist_max / total_dist
-        normal_dist_loss = normal_dist_loss * args.dist_loss_weight
-        mu = None
-        cov = None
+    normal_dist_loss = normal_dist_loss * args.dist_loss_weight
+
     return normal_dist_max, normal_dist_loss, mu, cov
