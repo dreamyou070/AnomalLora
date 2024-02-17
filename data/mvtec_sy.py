01fad239c9ad259e49976917d994b8164ab2e38b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-17 14:06:20 +0900

    Update mvtec_sy.py

--- data/mvtec_sy.py
+++ data/mvtec_sy.py
@@ -65,6 +65,7 @@ class MVTecDRAEMTestDataset(Dataset):
 
         return sample
 
+anomal_p = 0.05
 
 class MVTecDRAEMTrainDataset(Dataset):
 
@@ -173,7 +174,7 @@ class MVTecDRAEMTrainDataset(Dataset):
             # smoothing
             perlin_thr = cv2.GaussianBlur(perlin_thr, (5,5), 0)
             binary_2D_mask = (np.where(perlin_thr == 0, 0, 1)).astype(np.float32)  # [512,512,3]
-            if np.sum(binary_2D_mask) > 0.01 * total_object_pixel :
+            if np.sum(binary_2D_mask) > anomal_p * total_object_pixel :
                 break
         perlin_thr = np.expand_dims(perlin_thr, axis=2)  # [512,512,3]
         # if i rase beta_scale_factor,
@@ -198,7 +199,7 @@ class MVTecDRAEMTrainDataset(Dataset):
                 result_thr = (result_thr * object_position).astype(np.float32)
             blur_2D_mask = cv2.GaussianBlur(result_thr, (3,3), 0)
             binary_2D_mask = (np.where(blur_2D_mask == 0, 0, 1)).astype(np.float32)  # [512,512,3]
-            if np.sum(binary_2D_mask) > 0.01 * total_object_pixel :
+            if np.sum(binary_2D_mask) > anomal_p * total_object_pixel :
                 break
         blur_3D_mask = np.expand_dims(blur_2D_mask, axis=2)  # [512,512,3]
         A = back_img.astype(np.float32)  # merged
