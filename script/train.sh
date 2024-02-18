c7b0488b4cd662617939da080ba89a59c6b173a8
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 00:50:31 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=50102
+port_number=50103
 
 obj_name='carrot'
 trigger_word='carrot'
@@ -9,9 +9,9 @@ anomal_source_path="../../../MyData/anomal_source"
 #--do_dist_loss
 # --attention_loss_check_third
 # --do_map_loss
-accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
+accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/22_2_do_anomal_do_holed_sample_do_normal_sample_do_attn_loss_do_dist_loss_second_attn_gen_code" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/22_3_do_anomal_do_holed_sample_do_normal_sample_do_attn_loss_do_dist_loss_do_map_loss_second_attn_gen_code" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --beta_scale_factor 0.8 --anomal_source_path "${anomal_source_path}" --anomal_only_on_object --bgrm_test \
@@ -20,4 +20,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_config \
  --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_anomal_sample --do_holed_sample --do_normal_sample \
  --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1.0 \
- --do_dist_loss
+ --do_dist_loss \
+ --do_map_loss
