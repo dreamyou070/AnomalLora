0966b42a9880ec26f6dd94000c6e43511371c720
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-18 11:21:48 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -3,7 +3,7 @@
 port_number=50003
 obj_name='carrot'
 caption='carrot'
-folder_name="1_3_train_anomalous_detector_not_small_anomal_use_normal_attention_mask_self"
+folder_name="0_base"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 # --do_local_self_attn --window_size 8
