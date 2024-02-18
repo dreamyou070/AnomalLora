47142441df4c88f5fdbc25af18a0b5bd3afdf88d
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-18 20:05:44 +0900

    Update train.sh

--- script/train.sh
+++ script/train.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 
-port_number=50206
+port_number=50205
 
 obj_name='carrot'
 trigger_word='carrot'
@@ -9,7 +9,7 @@ anomal_source_path="../../../MyData/anomal_source"
 
 accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --main_process_port $port_number ../train.py --log_with wandb \
- --output_dir "../../result/${bench_mark}/${obj_name}/20_6_only_anomal_hole_normal_weight_0" \
+ --output_dir "../../result/${bench_mark}/${obj_name}/20_5_only_anomal_hole" \
  --train_unet --train_text_encoder --start_epoch 0 --max_train_epochs 30 \
  --pretrained_model_name_or_path ../../../pretrained_stable_diffusion/stable-diffusion-v1-5/v1-5-pruned.safetensors \
  --beta_scale_factor 0.8 --anomal_source_path "${anomal_source_path}" --anomal_only_on_object --bgrm_test \
@@ -17,5 +17,5 @@ accelerate launch --config_file ../../../gpu_config/gpu_0_1_2_3_4_5_config \
  --position_embedding_layer 'down_blocks_0_attentions_0_transformer_blocks_0_attn1' --d_dim 320 --latent_res 64 \
  --do_anomal_hole \
  --do_dist_loss --dist_loss_weight 1.0 \
- --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 0.0 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
+ --do_attn_loss --attn_loss_weight 1.0 --do_cls_train --normal_weight 1.0 --trg_layer_list "['up_blocks_3_attentions_2_transformer_blocks_0_attn2']" \
  --do_map_loss --use_small_anomal
