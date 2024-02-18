f52c8bb02bce1900470f312e1246d5b1d6f44616
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 07:45:39 +0900

    Update reconstruction.sh

--- script/reconstruction.sh
+++ script/reconstruction.sh
@@ -1,9 +1,9 @@
 # !/bin/bash
 
-port_number=50504
+port_number=52121
 obj_name='carrot'
 caption='carrot'
-folder_name="22_4_do_anomal_do_holed_sample_do_normal_sample_do_attn_loss_do_map_loss_second_attn_gen_code"
+folder_name="21_2_do_anomal_do_holed_sample_do_attn_loss_do_dist_loss"
 bench_mark="MVTec3D-AD"
 position_embedding_layer="down_blocks_0_attentions_0_transformer_blocks_0_attn1"
 # --do_local_self_attn --window_size 8
