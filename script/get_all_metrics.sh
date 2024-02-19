7346ed6778339d8282b642c4d88c010769047d5b
dreamyou070 <68425947+dreamyou070@users.noreply.github.com>
2024-02-19 09:26:28 +0900

    Update get_all_metrics.sh

--- script/get_all_metrics.sh
+++ script/get_all_metrics.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 class_name="carrot"
-second_folder_name="21_1_2_do_anomal_do_holed_sample_do_attn_loss_second_attn_gen_code"
+second_folder_name="22_1_2_do_anomal_do_holed_sample_do_normal_sample_do_attn_loss_second_attn_gen_code"
 
 
 python ../evaluation/get_all_metrics.py \
